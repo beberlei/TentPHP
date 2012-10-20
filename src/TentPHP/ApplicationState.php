5eaddb6078a1ab347d33fb920f554af79a55ca1e
Benjamin Eberlei <kontakt@beberlei.de>
2012-10-20 12:50:51 +0200

    Document ApplicationState interface

--- src/TentPHP/ApplicationState.php
+++ src/TentPHP/ApplicationState.php
@@ -2,13 +2,64 @@
 
 namespace TentPHP;
 
+/**
+ * Saves the application and entity state required for authorization with a Test server.
+ */
 interface ApplicationState
 {
+    /**
+     * Given an entity url return the tent server urls this entity is managed on.
+     *
+     * Return false, if no servers have been saved for this entity yet.
+     *
+     * @param string
+     * @return array|false
+     */
     public function getServers($entityUrl);
+
+    /**
+     * Get the auth-configuration for an application and tent server pair.
+     *
+     * @param string $serverUrl
+     * @param Application $application
+     *
+     * @return ApplicationConfig
+     */
     public function getApplicationConfig($serverUrl, Application $application);
-    public function saveServers($entityUrl, $serverUrls);
+
+    /**
+     * Get the access token for an user entity and application config pair.
+     *
+     * @param string $entityUrl
+     * @param ApplicationConfig $config
+     *
+     * @return string
+     */
+    public function getUserAccessToken($entityUrl, ApplicationConfig $config);
+
+    /**
+     * Save the tent servers responsible for a given entity.
+     *
+     * @param string $entityUrl
+     * @param array $serverUrls
+     */
+    public function saveServers($entityUrl, array $serverUrls);
+
+    /**
+     * Save the application authorization config for a given server url.
+     *
+     * @param string $serverUrl
+     * @param ApplicationConfig $config
+     */
     public function saveApplicationConfig($serverUrl, ApplicationConfig $config);
-    public function getUserAccessToken($entityUrl, Application $application);
-    public function saveUserAccessToken($entityUrl, Application $application, $token);
+
+    /**
+     * Save the user access token for a given entity and application pair.
+     *
+     * @param string $entityUrl
+     * @param ApplicationConfig $config
+     * @param string $token
+     */
+    public function saveUserAccessToken($entityUrl, ApplicationConfig $config, $token);
 }
 
