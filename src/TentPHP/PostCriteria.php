aad1f5d120ca57bdfc8310521e611b6904bd96fc
Benjamin Eberlei <kontakt@beberlei.de>
2012-10-20 17:37:12 +0200

    PostCriteria

--- src/TentPHP/PostCriteria.php
+++ src/TentPHP/PostCriteria.php
@@ -2,7 +2,22 @@
 
 namespace TentPHP;
 
-class PostCriteria
+/**
+ * Criteria object for filtering posts
+ */
+class PostCriteria extends DataObject
 {
+    protected $data = array(
+        'since_id'         => null,
+        'before_id'        => null,
+        'since_id_entity'  => null,
+        'before_id_entity' => null,
+        'since_time'       => null,
+        'before_time'      => null,
+        'entity'           => null,
+        'mentioned_entity' => null,
+        'post_types'       => null,
+        'limit'            => null,
+    );
 }
 
